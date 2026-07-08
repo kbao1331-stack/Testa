07add2c7d11f7b7c9c27c1a8036ee4103fbebed5
kbao1331-stack <kbao1331@gmail.com>
2026-07-08 14:25:38 +0700

    Add files via upload

--- hkaito.py
+++ hkaito.py
@@ -1,20 +1,19 @@
-import os
-import sys
 import requests
-from time import sleep
-import math
-import time
 
-# Chọn chức năng
 try:
     with open("key.txt", "r") as f:
-        chon = float(f.read().strip())
-    
+        chon = f.read().strip()  # Đọc dạng string cho an toàn
+
     url_map = {
-        limitedrw: 'https://raw.githubusercontent.com/kbao1331-stack/Testa/refs/heads/main/rwv2.py'
+        "limitedrw" : 'https://raw.githubusercontent.com/kbao1331-stack/Testa/refs/heads/main/rwv2.py'
     }
     
     if chon in url_map:
         exec(requests.get(url_map[chon]).text)
     else:
         print("key sai")
+
+except FileNotFoundError:
+    print("Không tìm thấy file key.txt")
+except Exception as e:
+    print(f"Lỗi khác: {e}")
