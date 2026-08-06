56c8b68b099af6c09b2ae41644079f414fd99f01
kbao1331-stack <kbao1331@gmail.com>
2026-08-06 16:08:06 +0700

    Update hkaito.py

--- hkaito.py
+++ hkaito.py
@@ -5,7 +5,7 @@ try:
         chon = f.read().strip()  # Đọc dạng string cho an toàn
 
     url_map = {
-        "limitedrw" : 'https://raw.githubusercontent.com/kbao1331-stack/Testa/refs/heads/main/rwv2.py',
+        "limitedrw" : 'https://raw.githubusercontent.com/kbao1331-stack/Testa/refs/heads/main/add.py',
         "ducanhdz" : 'https://raw.githubusercontent.com/kbao1331-stack/Testa/refs/heads/main/sharelink.py'
     }
     
